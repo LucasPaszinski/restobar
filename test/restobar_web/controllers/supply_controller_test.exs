ed2da73a169373fa823cd7b5c70a97065d0b71e2
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2022-02-02 14:51:29 -0300

    Fix Tests

--- test/restobar_web/controllers/supply_controller_test.exs
+++ test/restobar_web/controllers/supply_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule RestobarWeb.SupplyControllerTest do
   use RestobarWeb.ConnCase
 
+  import Restobar.RestaurantsFixtures
   import Restobar.SuppliesFixtures
 
   alias Restobar.Supplies.Supply
@@ -18,22 +19,29 @@ defmodule RestobarWeb.SupplyControllerTest do
   @invalid_attrs %{description: nil, expires_at: nil, responsible: nil}
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    [
+      conn: put_req_header(conn, "accept", "application/json"),
+      restaurant_id: restaurant_fixture(%{}).id
+    ]
   end
 
   describe "index" do
-    test "lists all supplies", %{conn: conn} do
-      conn = get(conn, Routes.supply_path(conn, :index))
+    test "lists all supplies", %{conn: conn, restaurant_id: restaurant_id} do
+      conn = get(conn, Routes.restaurant_supply_path(conn, :index, restaurant_id))
       assert json_response(conn, 200)["data"] == []
     end
   end
 
   describe "create supply" do
-    test "renders supply when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.supply_path(conn, :create), supply: @create_attrs)
+    test "renders supply when data is valid", %{conn: conn, restaurant_id: restaurant_id} do
+      conn =
+        post(conn, Routes.restaurant_supply_path(conn, :create, restaurant_id),
+          supply: @create_attrs
+        )
+
       assert %{"id" => id} = json_response(conn, 201)["data"]
 
-      conn = get(conn, Routes.supply_path(conn, :show, id))
+      conn = get(conn, Routes.restaurant_supply_path(conn, :show, restaurant_id, id))
 
       assert %{
                "id" => ^id,
@@ -43,8 +51,12 @@ defmodule RestobarWeb.SupplyControllerTest do
              } = json_response(conn, 200)["data"]
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.supply_path(conn, :create), supply: @invalid_attrs)
+    test "renders errors when data is invalid", %{conn: conn, restaurant_id: restaurant_id} do
+      conn =
+        post(conn, Routes.restaurant_supply_path(conn, :create, restaurant_id),
+          supply: @invalid_attrs
+        )
+
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -52,11 +64,19 @@ defmodule RestobarWeb.SupplyControllerTest do
   describe "update supply" do
     setup [:create_supply]
 
-    test "renders supply when data is valid", %{conn: conn, supply: %Supply{id: id} = supply} do
-      conn = put(conn, Routes.supply_path(conn, :update, supply), supply: @update_attrs)
+    test "renders supply when data is valid", %{
+      conn: conn,
+      restaurant_id: restaurant_id,
+      supply: %Supply{id: id} = supply
+    } do
+      conn =
+        put(conn, Routes.restaurant_supply_path(conn, :update, restaurant_id, supply),
+          supply: @update_attrs
+        )
+
       assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
-      conn = get(conn, Routes.supply_path(conn, :show, id))
+      conn = get(conn, Routes.restaurant_supply_path(conn, :show, restaurant_id, id))
 
       assert %{
                "id" => ^id,
@@ -66,8 +86,16 @@ defmodule RestobarWeb.SupplyControllerTest do
              } = json_response(conn, 200)["data"]
     end
 
-    test "renders errors when data is invalid", %{conn: conn, supply: supply} do
-      conn = put(conn, Routes.supply_path(conn, :update, supply), supply: @invalid_attrs)
+    test "renders errors when data is invalid", %{
+      conn: conn,
+      restaurant_id: restaurant_id,
+      supply: supply
+    } do
+      conn =
+        put(conn, Routes.restaurant_supply_path(conn, :update, restaurant_id, supply),
+          supply: @invalid_attrs
+        )
+
       assert json_response(conn, 422)["errors"] != %{}
     end
   end
@@ -75,18 +103,18 @@ defmodule RestobarWeb.SupplyControllerTest do
   describe "delete supply" do
     setup [:create_supply]
 
-    test "deletes chosen supply", %{conn: conn, supply: supply} do
-      conn = delete(conn, Routes.supply_path(conn, :delete, supply))
+    test "deletes chosen supply", %{conn: conn, restaurant_id: restaurant_id, supply: supply} do
+      conn = delete(conn, Routes.restaurant_supply_path(conn, :delete, restaurant_id, supply))
       assert response(conn, 204)
 
       assert_error_sent 404, fn ->
-        get(conn, Routes.supply_path(conn, :show, supply))
+        get(conn, Routes.restaurant_supply_path(conn, :show, restaurant_id, supply))
       end
     end
   end
 
-  defp create_supply(_) do
-    supply = supply_fixture()
+  defp create_supply(%{restaurant_id: restaurant_id}) do
+    supply = supply_fixture(%{restaurant_id: restaurant_id})
     %{supply: supply}
   end
 end
